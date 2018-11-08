03e411eb50a8f366e1d0d419bec845b3f78cde5a
MyDuan <dy1226130575@163.com>
2018-11-08 20:48:07 +0900

    Add a safe exp function and change the parameters.

--- boost_function.py
+++ boost_function.py
@@ -1,10 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+UBOUND_ARGX_EXP = 700
+def _safe_exp(x):
+    if x < 700:
+        return np.exp(x)
+    else:
+        return np.exp(700)
+
 def boost_function(x, k, c1, c2, c3, c4):
-    exp = np.exp(c2 * (x / 100.0 - k - c4))
+    exp = _safe_exp(c2 * (x / 100.0 - k - c4))
     y = 1.0*(c3 + c1 * exp) / (1 + exp)
-    print("y:",y)
     if c3 > c1:
         t = c3
     else:
@@ -14,14 +20,14 @@ def boost_function(x, k, c1, c2, c3, c4):
         return y
     else:
         return -1
-        print("Invalid result: x=%s, y=%s", x, y)
 
 if __name__ == "__main__":
-    x = np.arange(73000) * 0.1
-    y = np.zeros(73000)
-    for i in range(73000):
-        y[i] = boost_function(x[i], -200.0, 3.8, 50.0, 2.15, 260.0)
+    x = np.arange(500000) * 0.0001
+    y = np.zeros(500000)
+    for i in range(500000):
+        y[i] = boost_function(x[i], -121.79645156272358, 3.0, 10.0, 2.8, 13.7)
     fig = plt.figure()
-    plt.legend(loc='upper left')
+    plt.title("spot_score-boost function")
     plt.plot(x, y)
+    plt.legend(loc='upper left')
     plt.show()
