0fb916c7211d6f7b81c5cea5a2c1ba1db86fe46b
MyDuan <dy1226130575@163.com>
2019-02-28 17:31:48 +0900

    Add Bandit class model.

---
+++ ReinforcementLearning/Bandit.py
@@ -0,0 +1,73 @@
+import numpy as np
+
+class Bandit:
+
+    def __init__(self, k_arm=10, epsilon=0., initial=0., step_size=0.1, sample_averages=False, UCB_param=None,
+                    gradient=False, gradient_baseline=False, true_reward=0.):
+        self.k = k_arm
+        self.step_size = step_size
+        self.sample_averages = sample_averages
+        self.indices = np.arange(self.k)
+        self.time = 0
+        self.UCB_param = UCB_param
+        self.gradient = gradient
+        self.gradient_baseline = gradient_baseline
+        self.average_reward = 0
+        self.true_reward = true_reward
+        self.epsilon = epsilon
+        self.initial = initial
+
+    def reset(self):
+        # real reward for each action q*
+        self.q_true = np.random.randn(self.k) + self.true_reward
+
+        # estimation for each action Q
+        self.q_estimation = np.zeros(self.k) + self.initial
+
+        # of chosen times for each action
+        self.action_count = np.zeros(self.k)
+        # greedy
+        self.best_action = np.argmax(self.q_true)
+
+    # get an action for this bandit
+    def act(self):
+        if np.random.rand() < self.epsilon:
+            return np.random.choice(self.indices)
+
+        if self.UCB_param is not None:
+            UCB_estimation = self.q_estimation + \
+                     self.UCB_param * np.sqrt(np.log(self.time + 1) / (self.action_count + 1e-5))
+            q_best = np.max(UCB_estimation)
+            return np.random.choice([action for action, q in enumerate(UCB_estimation) if q == q_best])
+
+        if self.gradient:
+            exp_est = np.exp(self.q_estimation)
+            self.action_prob = exp_est / np.sum(exp_est)
+            return np.random.choice(self.indices, p=self.action_prob)
+
+        q_best = np.max(self.q_estimation)
+        return np.random.choice([action for action, q in enumerate(self.q_estimation) if q == q_best])
+
+    # take an action, update estimation for this action
+    def step(self, action):
+        # generate the reward under N(real reward, 1)
+        reward = np.random.randn() + self.q_true[action]
+        self.time += 1
+        self.average_reward = (self.time - 1.0) / self.time * self.average_reward + reward / self.time
+        self.action_count[action] += 1
+
+        if self.sample_averages:
+            # update estimation using sample averages
+            self.q_estimation[action] += 1.0 / self.action_count[action] * (reward - self.q_estimation[action])
+        elif self.gradient:
+            one_hot = np.zeros(self.k)
+            one_hot[action] = 1
+            if self.gradient_baseline:
+                baseline = self.average_reward
+            else:
+                baseline = 0
+            self.q_estimation = self.q_estimation + self.step_size * (reward - baseline) * (one_hot - self.action_prob)
+        else:
+            # update estimation with constant step size
+            self.q_estimation[action] += self.step_size * (reward - self.q_estimation[action])
+        return reward
